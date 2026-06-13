1b4e0d0b784564616f3205c8eaeb53e39ac6f0c3
lovesonbk30-ui <lovesonbk30@gmail.com>
2026-06-13 12:29:30 +0545

    Update ToDo_List.py

--- ToDo_List.py
+++ ToDo_List.py
@@ -5,20 +5,10 @@ import os
 import psycopg2
 
 # Initialize the Flask application
-app = Flask(__name__)
-database_url = os.environ.get("DATABASE_URL")
+app = Flask(__name__) # configure the SQLite database, relative to the app instance folder 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Loveson.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
-if database_url:
-    if database_url.startswith("postgres://"):
-        database_url = database_url.replace(
-            "postgres://",
-            "postgresql://",
-            1
-        )
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
-else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Loveson.db"
-    
 db = SQLAlchemy(app)
 
 
